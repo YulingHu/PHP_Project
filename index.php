650c5641cdf1be06f2965fbd52d702309bdab14e
YulingHu <yulinghu1018@gmail.com>
2021-01-14 00:47:08 -0500

    Create project skeleton

--- index.php
+++ index.php
@@ -1,7 +1,120 @@
 <html>
+
 	<head>
+		<link rel="stylesheet" href="index.css" type="text/css" />
+		<script type="text/javascript" src="index.js"></script>
+		<title>Sales Dashboard</title>
 	</head>
+
 	<body>
-		<h1>Hello World</h1>
+		<div class="outer-margin">
+			<div class="inner-margin">
+				<h1>Sales Dashboard</h1>
+				<main role="main">
+					<section>
+						<h2>Statistics</h2>
+						<div>
+							<ul>
+								<li>
+									<div>
+										<div>300.00 <span>CAD</span></div>
+										<div>Total Sales</div>
+									</div>
+								</li>
+								<li>
+									<div>
+										<div>200.00 <span>CAD</span></div>
+										<div>Total Tax</div>
+									</div>
+								</li>
+								<li>
+									<div>
+										<div>100.00 <span>CAD</span></div>
+										<div>Total Shipping</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+						<div>
+							<h3>Top Locations by Sales</h3>
+							<ol>
+								<li>
+									<div>Toronto <span>Canada</span></div>
+									<div>100.00 <span>CAD</span></div>
+								</li>
+								<li>
+									<div>Brandon <span>Canada</span></div>
+									<div>100.00 <span>CAD</span></div>
+								</li>
+								<li>
+									<div>Oakville <span>Canada</span></div>
+									<div>100.00 <span>CAD</span></div>
+								</li>
+							</ol>
+						</div>
+					</section>
+					<section>
+						<h2>Charts</h2>
+						<div>
+							<h3>Sales over Time</h3>
+						</div>
+						<div>
+							<h3>Sales by Location</h3>
+						</div>
+						<div>
+							<h3>Sales by Customer</h3>
+						</div>
+					</section>
+					<section>
+						<h2>Tables</h2>
+						<div>
+							<table>
+								<thead>
+									<tr>
+										<th>Order ID</th>
+										<th>Date</th>
+										<th>Customer Name</th>
+										<th>City</th>
+										<th>Tax</th>
+										<th>Shipping</th>
+										<th>Total</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>1365</td>
+										<td>9/13/2018</td>
+										<td>Daniel</td>
+										<td>Toronto</td>
+										<td>21.99</td>
+										<td>15</td>
+										<td>100.84</td>
+									</tr>
+									<tr>
+										<td>1364</td>
+										<td>9/12/2018</td>
+										<td>Matt</td>
+										<td>Brandon</td>
+										<td>10.63</td>
+										<td>15</td>
+										<td>58.9</td>
+									</tr>
+									<tr>
+										<td>1363</td>
+										<td>9/11/2018</td>
+										<td>Lisa</td>
+										<td>Toronto</td>
+										<td>8.01</td>
+										<td>15</td>
+										<td>64.68</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</section>
+				</main>
+			</div>
+		</div>
 	</body>
+
 </html>
